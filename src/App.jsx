825d3032f9577fb76b8aa0a9adc31ad74fd70a47
fabioadelfio <adelfiofabio07@gmail.com>
2025-05-29 15:51:52 +0200

    new post + bonus

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ export default function App() {
     public: false,
   });
 
+  const [alertMessage, setAlertMessage] = useState(``);
+
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     setFormData({
@@ -26,9 +28,11 @@ export default function App() {
     axios.post(apiUrl, formData)
       .then(response => {
         console.log(`Dati inviati correttamente: `, response.data);
+        setAlertMessage(`Post inviato con successo!`);
       })
       .catch(error => {
         console.log(`Errore durante l'invio dei dati `, error);
+        setAlertMessage(`Errore durante l'invio del post!`);
       });
   };
 
@@ -38,8 +42,10 @@ export default function App() {
     <>
       <h1 className="text-center text-white my-4">CREA UN NUOVO POST</h1>
 
-      <form className="container d-flex flex-wrap justify-content-evenly" onSubmit={handleSubmit}>
-        <div className="col-4 my-2">
+      {alertMessage && <div className="d-flex justify-content-center my-5">{alertMessage}</div>}
+
+      <form className="container d-flex flex-wrap justify-content-between" onSubmit={handleSubmit}>
+        <div className="col-4 my-2 w-50">
           <label htmlFor="" className="form-label fw-bold fs-4">Author:</label>
           <input 
             type="text" 
@@ -51,31 +57,31 @@ export default function App() {
           />
         </div>
 
-        <div className="col-4 my-2">
+        <div className="col-4 my-2 w-50">
           <label htmlFor="" className="form-label fw-bold fs-4">Title:</label>
           <input 
             type="text" 
-            name="author" 
+            name="title" 
             onChange={handleChange}
-            value={formData.author}
-            id="author" 
+            value={formData.title}
+            id="title" 
             className="form-control"
           />
         </div>
 
-        <div className="col-4 my-2">
+        <div className="col-6 my-2">
           <label htmlFor="" className="form-label fw-bold fs-4">Body:</label>
-          <input 
+          <textarea 
             type="text" 
-            name="author" 
+            name="body" 
             onChange={handleChange}
-            value={formData.author}
-            id="author" 
+            value={formData.body}
+            id="body" 
             className="form-control"
           />
         </div>
 
-        <div className="form-check">
+        <div className="form-check col-3 mt-5">
           <input 
             type="checkbox" 
             name="public" 
@@ -87,7 +93,7 @@ export default function App() {
           <label htmlFor="" className="form-check-label fw-bold fs-4 mx-3">Public</label>
         </div>
 
-        <button type="submit" className="btn btn-primary text-center px-4">Invia</button>
+        <button type="submit" className="btn btn-primary text-center px-4 col-4 my-3">Invia</button>
       </form>
     </>
   );
