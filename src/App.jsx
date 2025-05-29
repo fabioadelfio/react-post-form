fb3494cb7157a6b6db25abed227160b1d748bc9e
fabioadelfio <adelfiofabio07@gmail.com>
2025-05-29 15:39:35 +0200

    add forms handleSubmit handleChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,94 @@
-import Header from "./components/layouts/Header";
-import Main from "./components/layouts/Main";
-import Footer from "./components/layouts/Footer";
+import axios from "axios";
+import { useState } from "react";
 
 export default function App() {
+
+  const apiUrl = `https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts`;
+
+  const [formData, setFormData] = useState({
+    author: ``,
+    title: ``,
+    body: ``,
+    public: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === `checkbox` ? checked : value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post(apiUrl, formData)
+      .then(response => {
+        console.log(`Dati inviati correttamente: `, response.data);
+      })
+      .catch(error => {
+        console.log(`Errore durante l'invio dei dati `, error);
+      });
+  };
+
+
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <h1 className="text-center text-white my-4">CREA UN NUOVO POST</h1>
+
+      <form className="container d-flex flex-wrap justify-content-evenly" onSubmit={handleSubmit}>
+        <div className="col-4 my-2">
+          <label htmlFor="" className="form-label fw-bold fs-4">Author:</label>
+          <input 
+            type="text" 
+            name="author" 
+            onChange={handleChange}
+            value={formData.author}
+            id="author" 
+            className="form-control"
+          />
+        </div>
+
+        <div className="col-4 my-2">
+          <label htmlFor="" className="form-label fw-bold fs-4">Title:</label>
+          <input 
+            type="text" 
+            name="author" 
+            onChange={handleChange}
+            value={formData.author}
+            id="author" 
+            className="form-control"
+          />
+        </div>
+
+        <div className="col-4 my-2">
+          <label htmlFor="" className="form-label fw-bold fs-4">Body:</label>
+          <input 
+            type="text" 
+            name="author" 
+            onChange={handleChange}
+            value={formData.author}
+            id="author" 
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-check">
+          <input 
+            type="checkbox" 
+            name="public" 
+            onChange={handleChange}
+            value={formData.public}
+            id="public" 
+            className="form-check-input fs-2"
+          />
+          <label htmlFor="" className="form-check-label fw-bold fs-4 mx-3">Public</label>
+        </div>
+
+        <button type="submit" className="btn btn-primary text-center px-4">Invia</button>
+      </form>
     </>
   );
 }
